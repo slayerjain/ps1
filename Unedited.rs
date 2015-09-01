78fbcc9cdec5561c2991ee82b39d860d18485c29
shubham_jain <shubhamkjain@outlook.com>
2015-09-02 00:44:52 +0530

    Added the required files

---
+++ Unedited.rs
@@ -0,0 +1,62 @@
+//
+// zhttpto.rs
+//
+// Starting code for PS1
+// Running on Rust 0.12
+//
+// Note that this code has serious security risks! You should not run it
+// on any system with access to sensitive files.
+//
+// University of Virginia - cs4414 Spring 2014
+// Weilin Xu and David Evans
+// Version 0.3
+
+use std::io::{Read, Write};
+use std::net::TcpListener;
+use std::str;
+use std::thread;
+
+fn main() {
+    let addr = "127.0.0.1:4414";
+
+    let listener = TcpListener::bind(addr).unwrap();
+
+    println!("Listening on [{}] ...", addr);
+
+    for stream in listener.incoming() {
+        match stream {
+            Err(_) => (),
+            Ok(mut stream) => {
+                // Spawn a thread to handle the connection
+                thread::spawn(move|| {
+                    match stream.peer_addr() {
+                        Err(_) => (),
+                        Ok(pn) => println!("Received connection from: [{}]", pn),
+                    }
+
+                    let mut buf = [0 ;500];
+                    let _ = stream.read(&mut buf);
+                    match str::from_utf8(&buf) {
+                        Err(error) => println!("Received request error:\n{}", error),
+                        Ok(body) => println!("Recieved request body:\n{}", body),
+                    }
+
+                    let response =
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+                         <doctype !html><html><head><title>Hello, Rust!</title>
+                         <style>body { background-color: #111; color: #FFEEAA }
+                                h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
+                                h2 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm green}
+                         </style></head>
+                         <body>
+                         <h1>Greetings, Krusty!</h1>
+                         </body></html>\r\n";
+                    let _ = stream.write(response.as_bytes());
+                    println!("Connection terminates.");
+                });
+            },
+        }
+    }
+
+    drop(listener);
+}
